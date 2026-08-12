a83e1f82c2c96a054262c5d14156d23939a0a2de
Graciela Castillo <grcastillogu@est.utn.ac.cr>
2026-08-12 17:58:26 -0600

    Agregar clase Inquilino

---
+++ src/modelo/Inquilino.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author graci
+ */
+public class Inquilino {
+    private int cedInqui;
+    private String nomInqui;
+    private String genero;
+    private LocalDate fechNac;
+    private String direccion;
+    private String telefono;
+    private String email;
+    private String ocupacion;
+    
+    public Inquilino() {
+        this.cedInqui = 0;
+        this.nomInqui = "";
+        this.genero = "";
+        this.fechNac = LocalDate.now();
+        this.direccion = "";
+        this.telefono = "";
+        this.email = "";
+        this.ocupacion = "";
+    }
+
+    public Inquilino(int cedInqui, String nomInqui, String genero, LocalDate fechNac, String direccion, String telefono, String email, String ocupacion) {
+        this.cedInqui = cedInqui;
+        this.nomInqui = nomInqui;
+        this.genero = genero;
+        this.fechNac = fechNac;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+        this.ocupacion = ocupacion;
+    }
+
+    public int getCedInqui() {
+        return cedInqui;
+    }
+
+    public void setCedInqui(int cedInqui) {
+        this.cedInqui = cedInqui;
+    }
+
+    public String getNomInqui() {
+        return nomInqui;
+    }
+
+    public void setNomInqui(String nomInqui) {
+        this.nomInqui = nomInqui;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public LocalDate getFechNac() {
+        return fechNac;
+    }
+
+    public void setFechNac(LocalDate fechNac) {
+        this.fechNac = fechNac;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOcupacion() {
+        return ocupacion;
+    }
+
+    public void setOcupacion(String ocupacion) {
+        this.ocupacion = ocupacion;
+    }
+    
+    
+}
---
+++ src/modelo/Propietario.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author graci
+ */
+public class Propietario {
+    private int cedPropiet;
+    private String nomPropiet;
+    private String genero;
+    private String direccion;
+    private String telefono;
+    private String email;
+    private String ocupacion;
+
+    
+        public Propietario() {
+        this.cedPropiet = 0;
+        this.nomPropiet = "";
+        this.genero = "";
+        this.direccion = "";
+        this.telefono = "";
+        this.email = "";
+        this.ocupacion = "";
+    }
+        
+        public Propietario(int cedPropiet, String nomPropiet, String genero, String direccion, String telefono, String email, String ocupacion) {
+        this.cedPropiet = cedPropiet;
+        this.nomPropiet = nomPropiet;
+        this.genero = genero;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+        this.ocupacion = ocupacion;
+    }
+
+    public int getCedPropiet() {
+        return cedPropiet;
+    }
+
+    public void setCedPropiet(int cedPropiet) {
+        this.cedPropiet = cedPropiet;
+    }
+
+    public String getNomPropiet() {
+        return nomPropiet;
+    }
+
+    public void setNomPropiet(String nomPropiet) {
+        this.nomPropiet = nomPropiet;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOcupacion() {
+        return ocupacion;
+    }
+
+    public void setOcupacion(String ocupacion) {
+        this.ocupacion = ocupacion;
+    }
+    
+    @Override
+    public String toString(){
+        return nomPropiet;
+    }
+    
+}
